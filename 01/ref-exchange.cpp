4f350b099ee9ad6c286b26b7891f75f6209db45b
sml782 <sml782@163.com>
2020-08-09 19:30:57 +0800

    feat: add value change ref

---
+++ 01/ref-exchange.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void SwapValue(int a, int b) {
+  int temp = a;
+  a = b;
+  b = temp;
+  cout<<"在SwapValue()中:\t\ta="<<a<<", b="<<b<<endl;
+};
+
+void SwapRef(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+  cout<<"SwapRef()中:\t\ta="<<a<<", b="<<b<<endl;
+}
+
+int main()
+{
+  int a = 10;
+  int b = 20;
+  cout<<"数据交换前:\t\ta="<<a<<", b="<<b<<endl;
+  SwapValue(a, b);
+  cout<<"调用SwapValue()后:\t\ta="<<a<<", b="<<b<<endl;
+  a = 10;
+  b = 20;
+  SwapRef(a, b);
+  cout<<"SwapRef()后:\t\ta="<<a<<", b="<<b<<endl;
+  return 0;
+}
