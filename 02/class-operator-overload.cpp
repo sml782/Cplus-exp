4118ba40d85c02953a2daef1ef17d4369f425036
sml782 <sml782@163.com>
2020-08-21 18:21:11 +0800

    feat: 类添加运算符重载

---
+++ 02/class-operator-overload.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// 复数类
+class MyComplex {
+  private:
+    double real;
+    double imag;
+  public:
+    MyComplex();
+    MyComplex(double, double);
+    void outCom();
+    
+    // 开始重载
+    MyComplex operator-(const MyComplex &);
+    friend MyComplex operator+(const MyComplex &, const MyComplex &);
+};
+
+MyComplex::MyComplex(): real(0), imag(0) {}
+
+MyComplex::MyComplex(double r, double i): real(r), imag(i) {}
+
+void MyComplex::outCom() {
+  cout << "(" << real << ", " << imag << ")";
+}
+
+MyComplex MyComplex::operator-(const MyComplex &c) {
+  return MyComplex(this->real - c.real, this->imag - c.imag);
+}
+
+MyComplex operator+(const MyComplex &c1, const MyComplex &c2) {
+  return MyComplex(c1.real + c2.real, c1.imag + c2.imag);
+}
+
+int main() {
+  MyComplex c1(1, 2);
+  MyComplex c2(3, 4);
+  MyComplex res;
+
+  c1.outCom();
+  cout << " operator+ ";
+  c2.outCom();
+  cout << " = ";
+  res = c1 + c2;
+  res.outCom();
+  cout << endl;
+
+  c1.outCom();
+  cout << " operator- ";
+  c2.outCom();
+  cout << " = ";
+  res = c1 - c2;
+  res.outCom();
+  cout << endl;
+
+
+  return 0;
+}
