798e469bdf2ec59fb2442fc727a20e1b90a1362f
sml782 <sml782@163.com>
2020-08-26 19:14:31 +0800

    feat: 构造函数|析构函数 调用虚函数

---
+++ virtual/03.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class A {
+  public:
+    virtual void hello() {
+      cout << "A::hello()" << endl;
+    }
+
+    virtual void bye() {
+      cout << "A::bye()" << endl;
+    }
+};
+
+class B: public A {
+  public:
+    virtual void hello() {
+      cout << "A::hello()" << endl;
+    }
+
+    B() {
+      hello();
+    }
+
+    ~B() {
+      bye();
+    }
+};
+
+class C: public B {
+  public:
+    virtual void hello() {
+      cout << "A::hello()" << endl;
+    }
+};
+
+int main() {
+  C obj;
+  return 0;
+}
